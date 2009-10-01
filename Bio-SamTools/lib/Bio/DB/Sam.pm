97d642756ed71ab650f97ab08f71ab97b78b7425
Scott Cain <scott@scottcain.net>
2009-10-01 17:20:56 +0000

    Arggh!  Fixed a warning message so that it actually tells the truth :-)

--- Bio-SamTools/lib/Bio/DB/Sam.pm
+++ Bio-SamTools/lib/Bio/DB/Sam.pm
@@ -1219,7 +1219,7 @@ sub new {
     my $expand_flags  = $args{-expand_flags};
     my $split_splices = $args{-split} || $args{-split_splices};
 
-    -e $bam_path && -r _  or croak "$fa_path does not exist or is not readable";
+    -e $bam_path && -r _  or croak "$bam_path does not exist or is not readable";
     my $bam = Bio::DB::Bam->open($bam_path)      or croak "$bam_path open: $!";
 
     my $fai;
