5be2cfc786f2f3cdcaed2f3ff5f923ec76354dbb
Scott Cain <scott@scottcain.net>
2011-06-23 20:10:46 +0000

    bumping the version to 0.32

--- Chado/lib/Bio/DB/Das/Chado.pm
+++ Chado/lib/Bio/DB/Das/Chado.pm
@@ -100,7 +100,7 @@ use constant SEGCLASS => 'Bio::DB::Das::Chado::Segment';
 use constant MAP_REFERENCE_TYPE => 'MapReferenceType'; #dgg
 use constant DEBUG => 0;
 
-$VERSION = 0.31;
+$VERSION = 0.32;
 @ISA = qw(Bio::Root::Root Bio::DasI);
 
 =head2 new
