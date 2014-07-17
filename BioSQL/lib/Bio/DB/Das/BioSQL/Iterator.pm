5c8f45ecf20d1ceba4bb88983a0fc672191775d2
Chris Fields <cjfields@illinois.edu>
2014-07-16 23:46:53 -0500

    Revert "remove unrelated code, simplify directory"
    This reverts commit 05444da341621dad4133090e02f586494df114fc.

---
+++ BioSQL/lib/Bio/DB/Das/BioSQL/Iterator.pm
@@ -0,0 +1,21 @@
+=head1 NAME
+
+Bio::DB::Das::BioSQL::Iterator
+
+=cut
+
+package Bio::DB::Das::BioSQL::Iterator;
+
+sub new {
+  my $package  = shift;
+  my $features = shift;
+  return bless $features,$package;
+}
+
+sub next_seq {
+  my $self = shift;
+  return unless @$self;
+  return shift @$self;
+}
+
+1;
